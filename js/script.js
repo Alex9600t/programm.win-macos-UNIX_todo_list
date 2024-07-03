04c8f62e4fd1136a8742a67b82167b5f8d310278
Bduidan <85570800+Alex9600t@users.noreply.github.com>
2024-07-03 10:24:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+console.log("hello");
+// document.querySelector(".todo_list_conteiner_class_cont_0").addEventListener('click', () => {
+//     document.getElementsByClassName('todo_list_conteiner_cont_0')[0].style= "text-decoration: line-through;";
+// });
+document.querySelector(".todo_list_conteiner_class_cont_1").addEventListener('click', () => {
+    document.getElementsByClassName('todo_list_conteiner_cont_1')[0].style= "text-decoration: line-through;";
+});
+document.querySelector(".todo_list_conteiner_class_cont_2").addEventListener('click', () => {
+    document.getElementsByClassName('todo_list_conteiner_cont_2')[0].style= "text-decoration: line-through;";
+});
+document.querySelector(".todo_list_conteiner_class_cont_3").addEventListener('click', () => {
+    document.getElementsByClassName('todo_list_conteiner_cont_3')[0].style= "text-decoration: line-through;";
+});
+document.querySelector(".todo_list_conteiner_class_cont_4").addEventListener('click', () => {
+    document.getElementsByClassName('todo_list_conteiner_cont_4')[0].style= "text-decoration: line-through;";
+});
+document.querySelector(".todo_list_conteiner_class_cont_5").addEventListener('click', () => {
+    document.getElementsByClassName('todo_list_conteiner_cont_5')[0].style= "text-decoration: line-through;";
+});
