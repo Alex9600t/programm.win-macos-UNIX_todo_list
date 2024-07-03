cf8892a46a3fd3fb55fb7079de9647aed0962411
Bduidan <bduidan@Macbook-air-M2---Bduidan.local>
2024-07-03 11:11:52 +0300

    update

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ console.log("hello");
 //     document.getElementsByClassName('todo_list_conteiner_cont_0')[0].style= "text-decoration: line-through;";
 // });
 document.querySelector(".todo_list_conteiner_class_cont_1").addEventListener('click', () => {
-    document.getElementsByClassName('todo_list_conteiner_cont_1')[0].style= "text-decoration: line-through;";
+    document.getElementsByClassName('todo_list_conteiner_cont_1')[0].style= "text-decoration: line-through; border: 3px solid rgb(80, 170, 78);";
 });
 document.querySelector(".todo_list_conteiner_class_cont_2").addEventListener('click', () => {
-    document.getElementsByClassName('todo_list_conteiner_cont_2')[0].style= "text-decoration: line-through;";
+    document.getElementsByClassName('todo_list_conteiner_cont_2')[0].style= "text-decoration: line-through; border: 3px solid rgb(80, 170, 78);";
 });
 document.querySelector(".todo_list_conteiner_class_cont_3").addEventListener('click', () => {
-    document.getElementsByClassName('todo_list_conteiner_cont_3')[0].style= "text-decoration: line-through;";
+    document.getElementsByClassName('todo_list_conteiner_cont_3')[0].style= "text-decoration: line-through; border: 3px solid rgb(80, 170, 78);";
 });
 document.querySelector(".todo_list_conteiner_class_cont_4").addEventListener('click', () => {
-    document.getElementsByClassName('todo_list_conteiner_cont_4')[0].style= "text-decoration: line-through;";
+    document.getElementsByClassName('todo_list_conteiner_cont_4')[0].style= "text-decoration: line-through; border: 3px solid rgb(80, 170, 78);";
 });
 document.querySelector(".todo_list_conteiner_class_cont_5").addEventListener('click', () => {
-    document.getElementsByClassName('todo_list_conteiner_cont_5')[0].style= "text-decoration: line-through;";
+    document.getElementsByClassName('todo_list_conteiner_cont_5')[0].style= "text-decoration: line-through; border: 3px solid rgb(80, 170, 78);";
 });
